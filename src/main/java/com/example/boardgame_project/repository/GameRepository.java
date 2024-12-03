8a4dc227b06ba9e89507932ba4f52f2e975d87ff
a1lep <aile.pent@gmail.com>
2024-12-03 14:05:56 +0200

    changes in service and repository 3.12

--- src/main/java/com/example/boardgame_project/repository/GameRepository.java
+++ src/main/java/com/example/boardgame_project/repository/GameRepository.java
@@ -3,14 +3,23 @@ package com.example.boardgame_project.repository;
 import com.example.boardgame_project.model.Game;
 import com.example.boardgame_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     void deleteGameById(Long id);
-    List<Game> findGameByGamename(String gamename);
-    List<Game> findGameByAvailability(boolean availability);
-    List<Game> findGameByGametype(String gametype);
+
+    @Query("SELECT g FROM Game g WHERE g.gamename = :gamename AND g.status = :status")
+    List<Game> findGameByGamename(@Param("gamename") String gamename, @Param("status") String status);
+
+    @Query("SELECT g FROM Game g WHERE g.availability = :gamename AND g.status = :status")
+    List<Game> findGameByAvailability(@Param("availability")boolean availability, @Param("status") String status);
+
+    @Query("SELECT g FROM Game g WHERE g.gametype = :gamename AND g.status = :status")
+    List<Game> findGameByGametype(@Param("gametype")String gametype, @Param("status") String status);
+
     List<Game> findByUserUsername(String username);
     List<Game> findByStatus(String status);
 }
