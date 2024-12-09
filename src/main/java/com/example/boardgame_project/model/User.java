59b9a4c8453458e928540e7bf67858d50a06ab18
a1lep <aile.pent@gmail.com>
2024-12-10 00:41:10 +0200

    changes to getUserByGameId - to get the email of the user.

--- src/main/java/com/example/boardgame_project/model/User.java
+++ src/main/java/com/example/boardgame_project/model/User.java
@@ -17,7 +17,6 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
     private String name;
     private String username;
@@ -25,61 +24,6 @@ public class User {
     private String role;
     private String email;
 
-    @OneToMany(mappedBy = "user")  // mappedBy refers to the 'user' field in the Game entity
-    @JsonBackReference
-    private List<Game> games;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /*public List<Game> getGames() {
-        return games;
-    }
-
-    public void setGames(List<Game> games) {
-        this.games = games;
-    }*/
 }
 
 
